8dc34471d5d94b86ac27dcd2bb230ebbbab3a9ea
Xin Sun <ddupgs@gmail.com>
2024-06-27 14:35:40 +0800

    #21 addendum (#25)

--- ddupg-common/src/main/java/io/ddupg/common/convert/Converters.java
+++ ddupg-common/src/main/java/io/ddupg/common/convert/Converters.java
@@ -6,36 +6,35 @@ public class Converters {
 
   public static final Converter<Integer, byte[]> INT_BYTES_CONVERTER = Bytes::toBytes;
 
-  public static final BiConverter<Integer, byte[]> INT_BYTES_BICONVERTER = new BiConverter<Integer, byte[]>() {
-    @Override
-    public byte[] convert(Integer value) {
-      return Bytes.toBytes(value);
-    }
+  public static final Converter<byte[], Integer> BYTES_INT_CONVERTER = Bytes::toInt;
 
-    @Override
-    public Integer invert(byte[] bytes) {
-      return Bytes.toInt(bytes);
-    }
-  };
+  public static final BiConverter<Integer, byte[]> INT_BYTES_BICONVERTER =
+      biConverter(INT_BYTES_CONVERTER, BYTES_INT_CONVERTER);
 
   public static final BiConverter<byte[], Integer> BYTES_INT_BICONVERTER =
       new ReversedBiConverter<>(INT_BYTES_BICONVERTER);
 
   public static final Converter<Long, byte[]> LONG_BYTES_CONVERTER = Bytes::toBytes;
 
-  public static final BiConverter<Long, byte[]> LONG_BYTES_BICONVERTER = new BiConverter<>() {
-    @Override
-    public byte[] convert(Long value) {
-      return Bytes.toBytes(value);
-    }
+  public static final Converter<byte[], Long> BYTES_LONG_CONVERTER = Bytes::toLong;
 
-    @Override
-    public Long invert(byte[] bytes) {
-      return Bytes.toLong(bytes);
-    }
-  };
+  public static final BiConverter<Long, byte[]> LONG_BYTES_BICONVERTER =
+      biConverter(LONG_BYTES_CONVERTER, BYTES_LONG_CONVERTER);
 
   public static final BiConverter<byte[], Long> BYTES_LONG_BICONVERTER =
       new ReversedBiConverter<>(LONG_BYTES_BICONVERTER);
 
+  public static <T, R> BiConverter<T, R> biConverter(Converter<T, R> converter, Converter<R, T> inverter) {
+    return new BiConverter<T, R>() {
+      @Override
+      public R convert(T t) {
+        return converter.convert(t);
+      }
+
+      @Override
+      public T invert(R r) {
+        return inverter.convert(r);
+      }
+    };
+  }
 }
