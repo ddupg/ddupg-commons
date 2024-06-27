f7feefc479871a8808e99924570a99edbbc429c2
Xin Sun <ddupgs@gmail.com>
2024-06-27 14:19:31 +0800

    #21 Add bytes tool (#23)

---
+++ ddupg-common/src/test/java/io/ddupg/common/convert/TestConverters.java
@@ -0,0 +1,44 @@
+package io.ddupg.common.convert;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TestConverters {
+
+  @Test
+  public void testInt() {
+    ThreadLocalRandom rnd = ThreadLocalRandom.current();
+    BiConverter<Integer, byte[]> converter = Converters.INT_BYTES_BICONVERTER;
+    for (int i = 0; i < 100; i++) {
+      test(converter, rnd.nextInt());
+    }
+
+    test(converter, 0);
+    test(converter, 1);
+    test(converter, -1);
+    test(converter, Integer.MAX_VALUE);
+    test(converter, Integer.MIN_VALUE);
+  }
+
+  @Test
+  public void testLong() {
+    ThreadLocalRandom rnd = ThreadLocalRandom.current();
+    BiConverter<Long, byte[]> converter = Converters.LONG_BYTES_BICONVERTER;
+    for (int i = 0; i < 100; i++) {
+      test(converter, rnd.nextLong());
+    }
+
+    test(converter, 0L);
+    test(converter, 1L);
+    test(converter, -1L);
+    test(converter, Long.MAX_VALUE);
+    test(converter, Long.MIN_VALUE);
+  }
+
+  private <T, R> void test(BiConverter<T, R> converter, T value) {
+    R r = converter.convert(value);
+    Assert.assertEquals(value, converter.invert(r));
+  }
+}
